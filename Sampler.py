1c89d3e50655bf621b00d281339672c150938933
Dennis Tang <dennisvtang@gmail.com>
2020-12-04 23:58:26 -0800

    Get path to default voicemeeter folder

---
+++ Sampler.py
@@ -0,0 +1,5 @@
+import os
+
+# get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
+voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
+
