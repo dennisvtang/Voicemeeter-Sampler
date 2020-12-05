fe15b22b7deaebe8ed3a575e833f00300a562388
Dennis Tang <dennisvtang@gmail.com>
2020-12-04 23:58:49 -0800

    Get all soundbytes in voicemeeter folder

--- Sampler.py
+++ Sampler.py
@@ -3,3 +3,6 @@ import os
 # get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
 voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
 
+# get all soundbytes in voicemeeter folder
+soundbytes = [f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicemeeter_folder_path) if ".wav" in file]
+
