af51e843302c7ec693695ff66ec0f9523bd7f718
Dennis Tang <dennisvtang@gmail.com>
2020-12-04 23:59:09 -0800

    Get last recorded soundbyte

--- Sampler.py
+++ Sampler.py
@@ -6,3 +6,7 @@ voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
 # get all soundbytes in voicemeeter folder
 soundbytes = [f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicemeeter_folder_path) if ".wav" in file]
 
+# get last recorded soundbyte
+latest_file = max(soundbytes, key=os.path.getctime)
+print('latest file: ', latest_file)
+
