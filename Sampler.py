3b177a655c951594174042cdf60b532f8c1c1c2f
Dennis Tang <dennisvtang@gmail.com>
2020-12-05 00:54:48 -0800

    Turned into a function

--- Sampler.py
+++ Sampler.py
@@ -18,67 +18,79 @@ config_file.macro_buttons = []
 voicemeeter_config_file = f"{voicemeeter_folder_path}\\MacroButtonConfig.xml"
 with open(voicemeeter_config_file) as f:
     raw_data = "".join(f.readlines())
+
+def load_voicemeeter_macro_config(config_path:str):
+    '''Loads voicemeeter macro button config at given path and returns it as a VoicemeeterMacroMap object'''
     
-    # get xml_info
+    # create VoicemeeterMacroMap object to store all data
+    config_file = VoicemeeterMacroMap()
+    config_file.macro_buttons = []
+
+    # load raw string from xml file
+    with open(config_path) as f:
+        raw_data = "".join(f.readlines())
+        
+    # parse xml_info
     xml_info = re.search(r"<\?xml (.*?)\?>", raw_data).group(1).replace(" ", ", ")
     xml_info = f"dict({xml_info})"
     xml_info = eval(xml_info)
     config_file.xml_info = xml_info
 
-    # get MacroButtonConfiguration
+    # parse MacroButtonConfiguration
     MacroButtonConfiguration = re.search(r"<MacroButtonConfiguration (.*?) />", raw_data).group(1).replace(" ", ", ")
     MacroButtonConfiguration = f"dict({MacroButtonConfiguration})"
     MacroButtonConfiguration = eval(MacroButtonConfiguration)
     config_file.MacroButtonConfiguration = MacroButtonConfiguration
 
-    # get MacroButtons
+    # parse MacroButtons
     all_macro_buttons = re.findall(r"<MacroButton (.*?)</MacroButton>", raw_data, re.DOTALL|re.MULTILINE)
     # process each MacroButton
     row_of_buttons = []
-    for counter, mb in enumerate(all_macro_buttons):
-        # get macro_button_info
+    for mb in all_macro_buttons:
+        # parse macro_button_info
         macro_button_info = mb[:mb.find(">")].replace(" ", ", ")
         macro_button_info = f"dict({macro_button_info})"
         macro_button_info = eval(macro_button_info)
 
-        # get mb_midi
+        # parse mb_midi
         mb_midi = re.search(r"<MB_MIDI (.*?) />", mb).group(1).replace(" ", ", ")
         mb_midi = f"dict({mb_midi})"
         mb_midi = eval(mb_midi)
 
-        # get mb_trigger
+        # parse mb_trigger
         mb_trigger = re.search(r"<MB_TRIGGER (.*?) />", mb).group(1).replace(" ", ", ")
         mb_trigger = f"dict({mb_trigger})"
         mb_trigger = eval(mb_trigger)
         
-        # get mb_xinput
+        # parse mb_xinput
         mb_xinput = re.search(r"<MB_XINPUT (.*?) />", mb).group(1).replace(" ", ", ")
         mb_xinput = f"dict({mb_xinput})"
         mb_xinput = eval(mb_xinput)
         
-        # get mb_name
+        # parse mb_name
         mb_name = re.search(r"<MB_Name>(.*?)</MB_Name>", mb).group(1)
 
-        # get mb_subname
+        # parse mb_subname
         mb_subname = re.search(r"<MB_Subname>(.*?)</", mb).group(1)
         
-        # get mb_init_request
+        # parse mb_init_request
         mb_init_request = re.search(r"<MB_InitRequest>(.*?)</", mb, re.DOTALL|re.MULTILINE).group(1)
         
-        # get mb_on_request
+        # parse mb_on_request
         mb_on_request = re.search(r"<MB_OnRequest>(.*?)</", mb, re.DOTALL|re.MULTILINE).group(1)
         
-        # get mb_off_request
+        # parse mb_off_request
         mb_off_request = re.search(r"<MB_OffRequest>(.*?)</", mb, re.DOTALL|re.MULTILINE).group(1)
 
         # encapsulate MacroButton
         button = Macrobutton(macro_button_info, mb_midi, mb_trigger, mb_xinput, mb_name, mb_subname, mb_init_request, mb_on_request, mb_off_request)
         
         # convert button index into 2D array indexing
-        raw_index = button.macro_button_info['index']
         if len(row_of_buttons) <= 9:
             row_of_buttons.append(button)
         else:
             config_file.macro_buttons.append(row_of_buttons)
             row_of_buttons = []
+    
+    return config_file
 
