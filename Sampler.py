af5d750bc1b87950c995983d843e4e9477ed2f2e
Dennis Tang <dennisvtang@gmail.com>
2020-12-04 23:59:48 -0800

    Load xml info from config file

--- Sampler.py
+++ Sampler.py
@@ -1,5 +1,6 @@
 import os
 from DataStructures import VoicemeeterMacroMap, Macrobutton
+import re
 
 # get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
 voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
@@ -10,4 +11,16 @@ soundbytes = [f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicem
 # get last recorded soundbyte
 latest_file = max(soundbytes, key=os.path.getctime)
 print('latest file: ', latest_file)
+
+config_file = VoicemeeterMacroMap()
+# load MacroButtonConfig file
+voicemeeter_config_file = f"{voicemeeter_folder_path}\\MacroButtonConfig.xml"
+with open(voicemeeter_config_file) as f:
+    raw_data = "".join(f.readlines())
+    
+    # get xml_info
+    xml_info = re.search(r"<\?xml (.*?)\?>", raw_data).group(1).replace(" ", ", ")
+    xml_info = f"dict({xml_info})"
+    xml_info = eval(xml_info)
+    config_file.xml_info = xml_info
 
