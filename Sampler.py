c915ab4b1ab06c3cae0f6e6c85ed53615cef89be
Dennis Tang <dennisvtang@gmail.com>
2020-12-05 00:55:46 -0800

    Changed file to have if name == main structure

--- Sampler.py
+++ Sampler.py
@@ -2,22 +2,6 @@ import os
 from DataStructures import VoicemeeterMacroMap, Macrobutton
 import re
 
-# get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
-voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
-
-# get all soundbytes in voicemeeter folder
-soundbytes = [f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicemeeter_folder_path) if ".wav" in file]
-
-# get last recorded soundbyte
-latest_file = max(soundbytes, key=os.path.getctime)
-print('latest file: ', latest_file)
-
-config_file = VoicemeeterMacroMap()
-config_file.macro_buttons = []
-# load MacroButtonConfig file
-voicemeeter_config_file = f"{voicemeeter_folder_path}\\MacroButtonConfig.xml"
-with open(voicemeeter_config_file) as f:
-    raw_data = "".join(f.readlines())
 
 def load_voicemeeter_macro_config(config_path:str):
     '''Loads voicemeeter macro button config at given path and returns it as a VoicemeeterMacroMap object'''
@@ -94,3 +78,21 @@ def load_voicemeeter_macro_config(config_path:str):
     
     return config_file
 
+
+
+if __name__ == "__main__":
+    # get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
+    voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
+
+    # get all soundbytes in voicemeeter folder
+    soundbytes = [f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicemeeter_folder_path) if ".wav" in file]
+
+    # get last recorded soundbyte
+    latest_file = max(soundbytes, key=os.path.getctime)
+    print('latest file: ', latest_file)
+
+    # load MacroButtonConfig file
+    config_path = f"{voicemeeter_folder_path}\\MacroButtonConfig.xml"
+    config_file = load_voicemeeter_macro_config(config_path)
+
+    print(config_file.macro_buttons[0][5].mb_name)
