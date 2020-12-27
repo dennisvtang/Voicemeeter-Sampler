7e0403525baf14798f608b451f06c02e34656080
dennisvtang <dennisvtang@gmail.com>
2020-12-26 20:23:16 -0800

    Add check if documents folder is located within OneDrive folder

--- Sampler.py
+++ Sampler.py
@@ -106,7 +106,10 @@ if __name__ == "__main__":
     col = int(sys.argv[2])
 
     # get path to default voicemeeter folder (where soundbytes are saved and where MacroButtonsConfig is saved)
-    voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
+    voicemeeter_folder_path = os.path.expanduser("~\OneDrive\Documents\\Voicemeeter")
+    # add check for if documents are saved in OneDrive folder
+    if os.path.exists(voicemeeter_folder_path) == False:
+        voicemeeter_folder_path = os.path.expanduser("~\Documents\\Voicemeeter")
 
     # get all soundbytes in voicemeeter folder
     soundbytes = set([f"{voicemeeter_folder_path}\\{file}" for file in os.listdir(voicemeeter_folder_path) if ".wav" in file])
